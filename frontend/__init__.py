9c78b6be21cafbd3125502ae4a5f50e9e4eaf501
erik <e.m.schepers@gmail.com>
2020-01-19 15:36:49 +0100

    Improvements, and added function to retrieve transactions

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import logging
+from decimal import Decimal
 
+from attr import dataclass
 from flask import Flask, request
 from flask_restful import Resource, Api
 from json import dumps
@@ -8,7 +10,6 @@ from flask_cors import CORS
 
 import application.services
 from domain.account_management.model.category import category_repository
-
 
 logger = logging.getLogger(__name__)
 
@@ -18,13 +19,14 @@ api = Api(app)
 
 
 class Combined:
-    def __init__(self, date, balance, income, expenses, profit, loss):
+    def __init__(self, date, balance, income, expenses, profit, loss, category_amount):
         self.date = date
         self.balance = balance
         self.income = income
         self.expenses = expenses
         self.profit = profit
         self.loss = loss
+        self.category_amount = category_amount
 
 
 class Balance:
@@ -33,16 +35,35 @@ class Balance:
         self.amount = amount
 
 
+@dataclass
 class Category:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
+    id: str
+    name: str
+
+
+# class Category:
+#     def __init__(self, id, name):
+#         self.id = id
+#         self.name = name
+
+
+@dataclass
+class Transaction:
+    id: str
+    date: datetime.date
+    account: str
+    amount: Decimal
+    name: str
+    category: Category
+    description: str
+    counter_account: str
+    internal: bool
 
 
 @app.route('/categories')
 def get_categories():
     categories = [Category(0, "None").__dict__] + [Category(cat.id, cat.qualified_name).__dict__ for cat in
-                                          category_repository().get_categories()]
+                                                   category_repository().get_categories()]
     response = app.response_class(dumps(categories, default=str),
                                   mimetype='application/json')
     return response
@@ -67,13 +88,13 @@ def get_category_data(category_id):
 def get_combined_category_data_for_period(parent_id=None):
     try:
         start = request.args.get('start', None)
-        start_date = datetime.fromtimestamp(int(float(start)/1000)).date()
+        start_date = datetime.fromtimestamp(int(float(start) / 1000)).date()
     except ValueError:
         start_date = application.services.get_date_of_first_transaction()
 
     try:
         end = request.args.get('end', None)
-        end_date = datetime.fromtimestamp(int(float(end)/1000)).date()
+        end_date = datetime.fromtimestamp(int(float(end) / 1000)).date()
     except ValueError:
         end_date = application.services.get_date_of_last_transaction()
 
@@ -110,15 +131,45 @@ def get_combined_category_data_for_period(parent_id=None):
 @app.route('/combined')
 def get_combined_data():
     mode = request.args.get('mode', 'monthly')
+    category_id = request.args.get('category', None)
+    category = category_repository().get_category(category_id)
     date_list = application.services.get_transaction_date_range(mode=mode)
     balance_list = [application.services.get_combined_balance_at(date) for date in date_list]
     income, expenses, profit, loss = application.services.get_income_expenses_profit_loss(date_list, mode=mode)
+    category_amounts = [application.services.get_combined_amount_for_category(category, d, mode) for d in date_list]
     response = app.response_class(
         response=dumps(
-            [Combined(date, balance, income, expenses, profit, loss).__dict__ for
-             (date, balance, income, expenses, profit, loss) in
-             zip(date_list, balance_list, income, expenses, profit, loss)],
+            [Combined(date, balance, income, expenses, profit, loss, category_amount).__dict__ for
+             (date, balance, income, expenses, profit, loss, category_amount) in
+             zip(date_list, balance_list, income, expenses, profit, loss, category_amounts)],
             default=str),
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/transactions')
+def get_transactions():
+    try:
+        start = request.args.get('start', None)
+        start_date = datetime.fromtimestamp(int(float(start) / 1000)).date()
+    except (ValueError, TypeError):
+        start_date = application.services.get_date_of_first_transaction()
+
+    try:
+        end = request.args.get('end', None)
+        end_date = datetime.fromtimestamp(int(float(end) / 1000)).date()
+    except (ValueError, TypeError):
+        end_date = application.services.get_date_of_last_transaction()
+
+    transactions = application.services.get_transactions(start_date, end_date)
+    response = app.response_class(
+        response=dumps(
+            [Transaction(t.id, t.date, t.account.name, t.amount, t.name, t.category and Category(t.category.id, t.category.qualified_name).__dict__ or None,
+                         t.description, t.counter_account, t.internal).__dict__
+             for t in transactions],
+            default=str
+        ),
         mimetype='application/json'
     )
     return response
